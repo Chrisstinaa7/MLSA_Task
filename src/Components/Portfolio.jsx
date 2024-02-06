ce453e2e0237680b7ee222c6d4273c17f7fcb71c
Chrisstinaa7 <123315047+Chrisstinaa7@users.noreply.github.com>
2024-02-06 19:20:35 +0500

    add all changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/computer.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "desk with laptop";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Gym Landing Page🎉",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Start your fitness journey with FitFusion's beginners friendly fitness plans.",
+    url: "https://chrisstinaa7.github.io/FitFusion/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "BMI calculator",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "A Python project for calculating Body Mass Index (BMI) with user input validation and results display.",
+    url: "https://chrisstinaa7.github.io/BMI-Calculator/",
   },
   {
-    title: "My Resume Site",
+    title: "My Portfolio",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Explore my portfolio to know all about me and my projects.",
+    url: "https://github.com/Chrisstinaa7/CodSoft/tree/main/Portfolio%20-%20Task%202%20-%20Level%201",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Simple Calculator Site",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "A straightforward web-based calculator for basic arithmetic operations.",
+    url: "https://github.com/Chrisstinaa7/CodSoft/tree/main/Calculator%20-%20Task%203%20-%20Level%201",
   },
 ];
 
