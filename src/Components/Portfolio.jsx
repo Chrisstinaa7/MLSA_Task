349297da263f7fd6a24ff734fe00841eb644d447
Chrisstinaa7 <123315047+Chrisstinaa7@users.noreply.github.com>
2024-02-06 19:51:52 +0500

    add all changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -34,13 +34,13 @@ const projectList = [
     title: "Gym Landing Page",
     description:
       "Start your fitness journey with FitFusion's beginners friendly fitness plans.",
-    url: "https://chrisstinaa7.github.io/FitFusion/",
+    url: "https://github.com/Chrisstinaa7/FitFusion/tree/main/Landing%20Page",
   },
   {
     title: "BMI calculator",
     description:
       "A Python project for calculating Body Mass Index (BMI) with user input validation and results display.",
-    url: "https://chrisstinaa7.github.io/BMI-Calculator/",
+    url: "https://github.com/Chrisstinaa7/BMI-Calculator/blob/main/main.py",
   },
   {
     title: "My Portfolio",
